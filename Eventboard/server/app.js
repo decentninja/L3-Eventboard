1576665e51a785ae0b023305ad9bff1fb1511e3f
andreas <encrypt.wave@gmail.com>
2013-02-27 20:20:00 +0100

    background color

--- Eventboard/server/app.js
+++ Eventboard/server/app.js
@@ -1,12 +1,32 @@
 Meteor.startup(function () {
-// code to run on server at startup
     if (events.find().count() === 0) {
-      var stuff= [{name:'Language Cafe - English/French', description:'An event where you can improve your language-skills in a relaxed environment.  Get to know other people with the same interest in language and culture! Lunch is provided.'},
-		  {name:'Ovve-Gasque', description:'Celebrate whatever you want with us! Just bring you finest clothing...Ovven!'},
-		  {name:'Lunch with Corporate America', description:'A talk about the future of Corporate America, learn money and eat lunch!'},
-		  {name: 'Dunder och Brak Barbeque festival! - Nubbedraget.', description: 'Likes barbeque? Wanna party? Come party with us! The grills will be lit at 16:00 and food should be sold shortly after. BYOB.'},
-		  {name: 'Game-night', description: 'We have board-games, cards, Magic and candy. What else do you need?'}];
-	
+      var stuff= [
+        {
+          "description" : "An event where you can improve your language-skills in a relaxed environment.  Get to know other people with the same interest in language and culture! Lunch is provided.",
+          "name" : "Language Cafe - English/French",
+          "color" : "#BDCCD4",
+        },
+        {
+          "description" : "Celebrate whatever you want with us! Just bring you finest clothing...Ovven!",
+          "name" : "Ovve-Gasque",
+          "color" : "#3FA9F5",
+        },
+        {
+          "description" : "A talk about the future of Corporate America, learn money and eat lunch!",
+          "name" : "Lunch with Corporate America",
+          "color" : "#FF931E",
+        },
+        {
+          "description" : "Likes barbeque? Wanna party? Come party with us! The grills will be lit at 16:00 and food should be sold shortly after. BYOB.",
+          "name" : "Dunder och Brak Barbeque festival! - Nubbedraget.",
+          "color" : "#7AC943",
+        },
+        {
+          "description" : "We have board-games, cards, Magic and candy. What else do you need?",
+          "name" : "Game-night",
+          "color" : "#FF7BAC",
+        }
+      ]
       for (var i = 0; i < stuff.length; i++)
         events.insert(stuff[i]);
     }
