6b814e8f82fc6cdf91e8d1e3c34d8427c6ec953b
Viktor Palmkvist <viktor@palmkvist.com>
2013-02-27 17:39:32 +0100

    Added basic support for masonry

--- Eventboard/client/script.js
+++ Eventboard/client/script.js
@@ -1,3 +1,17 @@
 Template.events.events = function() {
-	return events.find({})
+    return events.find({})
 }
+
+Template.events.rendered = function() {
+    if (!this._rendered) {
+        this._rendered = true
+        $('#event_grid').masonry({
+	    // options
+    	    itemSelector : '.event',
+	    columnWidth : 240
+        });
+    } else {
+        $('#event_grid').masonry('reload')
+    }
+}
+
