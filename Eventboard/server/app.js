35f2152f5fc4c519a52483993412c7cc8c73f872
Daniil Pintjuk <Danielpintjuk@gmail.com>
2013-02-27 17:17:56 +0100

    added dummy data loader to server/app.js

--- Eventboard/server/app.js
+++ Eventboard/server/app.js
@@ -1,3 +1,10 @@
 Meteor.startup(function () {
 // code to run on server at startup
+    if (events.find().count() === 0) {
+      var stuff= [{name:'stuff', description:'we will do stuff'},
+		  {name:'more stuff', description:'we will do more stuff'}];
+	
+      for (var i = 0; i < stuff.length; i++)
+        events.insert(stuff[i]);
+    }
 });
