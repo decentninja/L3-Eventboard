fa64fc8299c8d23e695e92d5cb5b8ba4e8df8be7
andreas <encrypt.wave@gmail.com>
2013-02-27 18:10:12 +0100

    css fix on events

--- Eventboard/client/script.js
+++ Eventboard/client/script.js
@@ -6,9 +6,7 @@ Template.events.rendered = function() {
     if (!this._rendered) {
         this._rendered = true
         $('#event_grid').masonry({
-	    // options
     	    itemSelector : '.event',
-	    columnWidth : 240
         });
     } else {
         $('#event_grid').masonry('reload')
