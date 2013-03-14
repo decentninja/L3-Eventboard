decentninja/L3-Eventboard

commit 7dabf3833ebee852e79bc89623504f0789ee7d97

fixed url tag bug in firefox (no more Meteor.autorun on the route call)

Eventboard/client/script.js
@@ -50,11 +50,13 @@ Template.header.allOtherTags = function() {
 Template.header.events = {
     "click .add-tag-tag": function(e) {
         Session.set("tags", Session.get("tags").concat(e.target.text))
+        updateUrl()
     },
     "click .remove-tag-tag": function(e) {
         Session.set("tags", Session.get("tags").filter(function(o) {
             return o != e.target.text
         }))
+        updateUrl()
     }
 }
 

Eventboard/client/urls.js
@@ -13,12 +13,12 @@ var Router = Backbone.Router.extend({
 
 var router = new Router()
 
-Meteor.autorun(function() {
+function updateUrl() {
 	var tags = Session.get("tags")
 	if(tags != undefined) {
 		router.navigate(tags.join("/"), true)
 	}
-})
+}
 
 Meteor.startup(function () {
 	Backbone.history.start({pushState: true});

commit 364f9e81e0b2962b8cdf3041f1580c2608242c7e

fixed backbutton broken by 7dabf38 (last)

Eventboard/client/script.js
@@ -50,12 +50,14 @@ Template.header.allOtherTags = function() {
 Template.header.events = {
     "click .add-tag-tag": function(e) {
         Session.set("tags", Session.get("tags").concat(e.target.text))
+        e.preventDefault()
         updateUrl()
     },
     "click .remove-tag-tag": function(e) {
         Session.set("tags", Session.get("tags").filter(function(o) {
             return o != e.target.text
         }))
+        e.preventDefault()
         updateUrl()
     }
 }